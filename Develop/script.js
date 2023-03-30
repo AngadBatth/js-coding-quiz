e721a08884a9082507f5fd5a793e8f23a576b14b
AngadBatth <andyybatth@gmail.com>
2023-03-30 17:16:55 -0400

    completed HTML layout + comments

---
+++ Develop/script.js
@@ -0,0 +1,29 @@
+var questionsList = [
+    {
+      question: "Inside which HTML element do we put the JavaScript?",
+      options: ["<scripting>", "<js>", "<script>", "<javascript>"],
+      answer: "<script>",
+    },
+    {
+      question: "How do you write 'Hello World' in an alert box?",
+      options: ["alert('Hello World');", "alertBox('Hello World');", "msgBox('Hello World');", "msg('Hello World');"],
+      answer: "alert('Hello World');",
+    },
+    {
+      question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+      options: ["if (i <> 5)", "if i <> 5", "if i =! 5 then", "if (i != 5)"],
+      answer: "if (i != 5)",
+    },
+    {
+      question: "How can you add a comment in a JavaScript?",
+      options: ["<!--This is a comment-->", "'This is a comment", "//This is a comment", "!This is a comment!"],
+      answer: "//This is a comment",
+    },
+    {
+      question: "Which event occurs when the user clicks on an HTML element?",
+      options: ["onchange", "onmouseclick", "onclick", "onmouseover"],
+      answer: "onclick",
+    },
+  ];
+
+  
